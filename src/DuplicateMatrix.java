74c98a4520c764b0269cf9d086cd8caac13fe06a
Saveen <saveen047@gmail.com>
2021-07-29 23:03:42 +0530

    update code

--- src/DuplicateMatrix.java
+++ src/DuplicateMatrix.java
@@ -1,10 +1,24 @@
+import java.util.HashMap;
+
 public class DuplicateMatrix {
     public static void main(String[] args) {
         int[][] arr={{11,22,33},{10,11,30},{40,50,11}};
-        for(int i=0;i< arr.length;i++){
-            for(int j=0;j<arr.length;j++){
-                
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int z=0;
+        for (int[] ints : arr) {
+            for (int j = 0; j < arr.length; j++) {
+                if (map.containsKey(ints[j])) {
+                    z = map.get(ints[j]);
+                    map.put(ints[j], z + 1);
+                } else {
+                    map.put(ints[j], 1);
+                }
             }
+        }
+        if(z>1){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
         }
     }
 }
